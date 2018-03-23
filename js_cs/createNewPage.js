6e859130a90b1f69e1d021cd253f4c73ae0f9d26
CarinaSchutte <carinaschutte@hotmail.com>
2018-03-23 17:17:22 +0100

    first setup 23-3

---
+++ js_cs/createNewPage.js
@@ -0,0 +1,13 @@
+
+const embrace = (function() {
+    function DOMify(htmlString) {
+      const template = document.createElement("template")
+      template.innerHTML = htmlString
+  
+      return template.content.firstElementChild
+    }
+  
+    return {
+      DOMify: DOMify
+    }
+  })()
